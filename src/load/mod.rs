ac45eb27d3d9f0c77d7cb1770ac18ad945094d75
Glenn Bitar <glennbitar@gmail.com>
2025-08-23 21:50:37 +0200

    Add load tool

---
+++ src/load/mod.rs
@@ -0,0 +1,99 @@
+use clap::Args;
+use clap::Subcommand;
+use fake::Fake;
+use rand::seq::SliceRandom;
+use sea_orm::ActiveValue::Set;
+use sea_orm::DatabaseConnection;
+use sea_orm::EntityTrait;
+
+use crate::entity::note;
+use crate::entity::note::Entity as Note;
+use crate::entity::users as user;
+use crate::entity::users::Entity as User;
+
+#[derive(Debug, Clone, Args)]
+pub struct Load {
+	#[command(subcommand)]
+	load: LoadInner,
+}
+
+pub async fn load(db: DatabaseConnection, load: Load) -> anyhow::Result<()> {
+	match load.load {
+		LoadInner::Users { count } => users(db, count).await,
+		LoadInner::Notes {
+			count,
+			exact,
+			max_users,
+		} => notes(db, count, exact, max_users).await,
+	}
+}
+
+async fn users(db: DatabaseConnection, count: u32) -> anyhow::Result<()> {
+	let faker = fake::faker::name::en::Name();
+	let users = (0..count).map(|_| {
+		let name: String = faker.fake();
+		println!("New user: {name}");
+		user::ActiveModel {
+			name: Set(name),
+			..Default::default()
+		}
+	});
+	let _res = User::insert_many(users).exec(&db).await?;
+	println!("Loaded {count} new users");
+	Ok(())
+}
+
+async fn notes(
+	db: DatabaseConnection,
+	count: u32,
+	exact: bool,
+	max_users: u32,
+) -> anyhow::Result<()> {
+	let title = fake::faker::lorem::en::Words(1..5);
+	let body = fake::faker::lorem::en::Sentences(1..3);
+	let mut users = User::find().all(&db).await?;
+	users.shuffle(&mut rand::rng());
+	users.truncate(usize::try_from(max_users).unwrap());
+
+	let notes = users
+		.iter()
+		.map(|user::Model { user_id, name: _ }| {
+			let count = if exact {
+				count
+			} else {
+				rand::random_range(0..=count)
+			};
+			(0..count).map(|_| {
+				let title = title.fake::<Vec<String>>().join(" ");
+				let body = body.fake::<Vec<String>>().join(" ");
+				note::ActiveModel {
+					user_id: Set(*user_id),
+					title: Set(title),
+					body: Set(body),
+					..Default::default()
+				}
+			})
+		})
+		.flatten();
+	let notes: Vec<_> = notes.collect();
+	let total = notes.len();
+	let _res = Note::insert_many(notes).exec(&db).await?;
+	println!("Loaded {total} new notes");
+	Ok(())
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum LoadInner {
+	Users {
+		#[arg(default_value = "1")]
+		count: u32,
+	},
+	Notes {
+		#[arg(default_value = "5")]
+		count: u32,
+		#[arg(long)]
+		exact: bool,
+		#[arg(long, default_value = "50")]
+		max_users: u32,
+	},
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-use std::env;
-
 use axum::serve;
+use clap::Parser;
+use clap::Subcommand;
 use dotenvy::dotenv;
 use sea_orm::Database;
+use sea_orm::DatabaseConnection;
 use tokio::net::TcpListener;
 use tokio::runtime;
+use tokio::spawn;
 
 use crate::web::State;
 use crate::web::router;
 
 mod entity;
+mod load;
 mod web;
 
 fn main() -> anyhow::Result<()> {
@@ -21,13 +24,41 @@ fn main() -> anyhow::Result<()> {
 		.block_on(async_main())
 }
 
+#[derive(Debug, Clone, Parser)]
+struct Cli {
+	#[command(subcommand)]
+	command: Option<Command>,
+	#[arg(long, env = "DATABASE_URL")]
+	database_url: String,
+}
+
+#[derive(Debug, Clone, Default, Subcommand)]
+enum Command {
+	#[default]
+	Server,
+	Load(load::Load),
+}
 async fn async_main() -> anyhow::Result<()> {
-	let db = Database::connect(env::var("DATABASE_URL")?).await?;
+	let Cli {
+		command,
+		database_url,
+	} = Cli::parse();
+	let db = Database::connect(&database_url).await?;
+
+	let command = command.unwrap_or_default();
+
+	match command {
+		Command::Server => server(db).await,
+		Command::Load(load) => load::load(db, load).await,
+	}
+}
+
+async fn server(db: DatabaseConnection) -> anyhow::Result<()> {
 	let state = State { db };
 	let router = router().with_state(state);
 	let listener = TcpListener::bind("0.0.0.0:12345").await?;
 	eprintln!("listening {}", listener.local_addr()?);
-	serve(listener, router).await?;
+	spawn(async move { serve(listener, router).await }).await??;
 
 	Ok(())
 }
